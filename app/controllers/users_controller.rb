1ee269f4579728a1793c8f9a11dca1b70f7ab15f
W3villa <vishalprakash.meaurya@w3villa.com>
2023-11-08 12:29:29 +0530

    add signOut btn

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:alert] = "User has been Successfully registered!"
+      redirect_to signin_path
+    else
+      flash[:alert] = "User already registered!"
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @current_user = current_user
+    @rooms = Room.public_rooms
+    @users = User.all_except(@current_user)
+    @room = Room.new
+    @message = Message.new
+    @room_name = get_name(@user, @current_user)
+    @single_room = Room.where(name: @room_name).first || Room.create_private_room([@user, @current_user], @room_name)
+    @messages = @single_room.messages
+
+    render "rooms/index"
+  end
+
+  private
+  def get_name(user1, user2)
+    users = [user1, user2].sort
+    "private_#{users[0].id}_#{users[1].id}"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
   after_create_commit { broadcast_append_to self.room }
+  validates :content, presence: true, length: { minimum: 1 }
 end
